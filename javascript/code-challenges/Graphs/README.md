842c8ee99e971a807023cfca70586801448f0ee3
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-11-04 20:30:46 -0700

    updated the Graphs data structure files

---
+++ javascript/code-challenges/Graphs/README.md
@@ -0,0 +1,16 @@
+# Graphs
+
+## What are Graphs?
+
+- a data structure that holds nodes a.k.a. vertices
+- all other data structures fit into this category.
+
+
+## Terminology
+- Direction: When an edge points from one vertex to another (uni-directional, biderectional)
+- Node/Vertex: an object or "container" that holds the "value" or values
+- Edge: connection between vertices. How many eges does a node have? (0 - as many vertexes as exist)
+- Degree: How many edges a vertex has.
+- A-cyclical: doex NOT loop via edges
+- Cyclical: DOES loop via edges. a.k.a. can hot the same vertex more than once while traversing
+- Neighbors: vertex connected by a single edge. One edge awaye. No more than one. Can have many neighbors.
---
+++ javascript/code-challenges/Graphs/graphs.js
@@ -0,0 +1,72 @@
+'use strict';
+
+class Vertex {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Edge {
+  constructor(vertex, weight = 0) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor() {
+    this._adjacencyList = new Map();
+  }
+
+  addVertex(vertex) {
+    this._adjacencyList.set(vertex, []);
+  }
+
+  addDirectedEdge(startVertex, endVertex, weight = 0) {
+    if (!this._adjacencyList.has(startVertex) || !this._adjacencyList.has(endVertex)) {
+      throw new Error('__ERROR__ Invalid Vertices');
+    }
+
+    const adjacencies = this._adjacencyList.get(startVertex);
+    adjacencies.push(new Edge(endVertex, weight));
+  }
+
+  getNeighbors(vertex) {
+    if (!this._adjacencyList.has(vertex)) {
+      throw new Error('__ERROR__ Invalid Vertex');
+    }
+
+    return [...this._adjacencyList.get(vertex)];
+  }
+
+  breadthFirst(startNode) {
+    const queue = [];
+    const visitedNodes = new Set();
+
+    queue.push(startNode);
+    visitedNodes.add(startNode);
+
+    while (queue.length) {
+      const currentNode = queue.shift();
+      const neighbors = this.getNeighbors(currentNode);
+
+      for (let neighbor of neighbors) {
+        const neighborNode = neighbor.vertex;
+
+        if (visitedNodes.has(neighborNode)) {
+          continue;
+        } else {
+          visitedNodes.add(neighborNode);
+        }
+        queue.push(neighborNode);
+      }
+    }
+
+    depthFirst(startNode) {
+      const visitedNodes = new Set();
+
+    return visitedNodes;
+  }
+}
+
+module.exports = { Vertex, Edge, Graph };
