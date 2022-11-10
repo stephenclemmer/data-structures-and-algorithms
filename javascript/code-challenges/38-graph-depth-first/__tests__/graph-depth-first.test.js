6c8245f98139e9a8f53f467691e319909da81132
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-11-09 21:48:21 -0800

    challenge 38 added.

---
+++ javascript/code-challenges/38-graph-depth-first/__tests__/graph-depth-first.test.js
@@ -0,0 +1,33 @@
+// require '@testing-library/jest-dom';
+// const { describe, it, expect } = require('eslint/lib/rule-tester/rule-tester');
+const { Graph, Vertex } = require('../../38-graph-depth-first/graph-depth-first.js');
+
+describe('Graphs', () => {
+  it('should traverse depth first', () => {
+    const graph = new Graph();
+    const pandora = new Vertex('Pandora');
+    const arendelle = new Vertex('Arendelle');
+    const metroville = new Vertex('Metroville');
+    const monstroplolis = new Vertex('Monstroplolis');
+    const naboo = new Vertex('Naboo');
+    const narnia = new Vertex('Narnia');
+    graph.addNode(pandora);
+    graph.addNode(arendelle);
+    graph.addNode(metroville);
+    graph.addNode(monstroplolis);
+    graph.addNode(naboo);
+    graph.addNode(narnia);
+    graph.addEdge(pandora, arendelle);
+    graph.addEdge(pandora, metroville);
+    graph.addEdge(arendelle, metroville);
+    graph.addEdge(arendelle, monstroplolis);
+    graph.addEdge(metroville, monstroplolis);
+    graph.addEdge(metroville, naboo);
+    graph.addEdge(metroville, narnia);
+    graph.addEdge(monstroplolis, naboo);
+    graph.addEdge(naboo, narnia);
+
+    expect(graph.depthFirst(pandora)).toBe(['Pandora', 'Arendelle', 'Metroville', 'Monstroplolis', 'Naboo', 'Narnia']);
+  });
+});
+
---
+++ javascript/code-challenges/38-graph-depth-first/graph-depth-first.js
@@ -0,0 +1,92 @@
+'use strict';
+
+class Vertex {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Edge {
+  constructor(vertex, weight = 0) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor() {
+    this._adjacencyList = new Map();
+  }
+
+  addNode(value) {
+    this._adjacencyList.set(value, []);
+  }
+
+  addEdge(startVertex, endVertex, weight = 0) {
+    if (!this._adjacencyList.has(startVertex) || !this._adjacencyList.has(endVertex)) {
+      throw new Error('__ERROR__ Invalid Vertices');
+    }
+
+    const adjacencies = this._adjacencyList.get(startVertex);
+    adjacencies.push(new Edge(endVertex, weight));
+  }
+
+  getNodes() {
+    return this._adjacencyList;
+  }
+
+  getNeighbors(vertex) {
+    if (!this._adjacencyList.has(vertex)) {
+      throw new Error('__ERROR__ Invalid Vertex');
+    }
+
+    return [...this._adjacencyList.get(vertex)];
+  }
+
+  size() {
+    return this._adjacencyList.size;
+  }
+
+  breadthFirst(startNode) {
+    const queue = [];
+    const visitedNodes = new Set();
+
+    queue.push(startNode);
+    visitedNodes.add(startNode);
+
+    while (queue.length) {
+      const currentNode = queue.shift();
+      const neighbors = this.getNeighbors(currentNode);
+
+      for (let neighbor of neighbors) {
+        const neighborNode = neighbor.vertex;
+
+        if (visitedNodes.has(neighborNode)) {
+          continue;
+        } else {
+          visitedNodes.add(neighborNode);
+        }
+        queue.push(neighborNode);
+      }
+    }
+  }
+
+  depthFirst(startNode) {
+    const visitedNodes = new Set();
+    const _traverseNeighbors = (node) => {
+      if (visitedNodes.has(node)) {
+        return;
+      } else {
+        visitedNodes.add(node);
+      }
+      const neighbors = this.getNeighbors(node);
+      for (let neighbor of neighbors) {
+        _traverseNeighbors(neighbor.vertex);
+      }
+    };
+    _traverseNeighbors(startNode);
+    return visitedNodes;
+  }
+}
+
+module.exports = { Vertex, Edge, Graph};
